e1f8cf78f854559ef0c2072c9f229328aead8e69
MarkMatyushchenko <48213625+MarkMatyushchenko@users.noreply.github.com>
2019-04-07 01:26:41 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -346,13 +346,13 @@ function get3TopItems(arr) {
  *   [ 1, '2' ] => 1
  */
 function getPositivesCount(arr) {
-   var count = 0;
+    var count = 0;
    for (let i=0; i < arr.length; i++){
-       if(arr[i] > 0){
-        count++;
+       if(Number.isInteger(arr[i]) ){
+       		if(arr[i] >0){
+             count++;
+            }
        }
-        
-    
    }
    return count;
 }
