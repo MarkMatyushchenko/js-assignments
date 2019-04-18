5b086337e35ddd7e7c86c1b19d38647058758167
MarkMatyushchenko <48213625+MarkMatyushchenko@users.noreply.github.com>
2019-04-18 20:03:58 +0300

    Update 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -225,10 +225,6 @@ function getRectangleString(width, height) {
 function encodeToRot13(str) {
     var lowCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    for(let i=0; i<26; i++)
-    {
-        if(str[i] ==    
-    }
     return lowCase;
 }
 
