48dd346d5e1db93c940d73f1ddb5a18c4c413b5e
MarkMatyushchenko <48213625+MarkMatyushchenko@users.noreply.github.com>
2019-04-19 01:52:17 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -271,8 +271,28 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
+//podpravit'
 function isCreditCardNumber(ccn) {
-    throw new Error('Not implemented');
+    
+    if (/[^0-9-\s]+/.test(ccn)) return false;
+
+
+    var nCheck = 0, nDigit = 0, bEven = false;
+    cnn = ccn.replace(/\D/g, "");
+
+    for (var n = cnn.length - 1; n >= 0; n--) {
+        var cDigit =cnn.charAt(n),
+            nDigit = parseInt(cDigit, 10);
+
+        if (bEven) {
+            if ((nDigit *= 2) > 9) nDigit -= 9;
+        }
+
+        nCheck += nDigit;
+        bEven = !bEven;
+    }
+
+    return (nCheck % 10) == 0;
 }
 
 
